a25c2a43563b143d24aff3ecd2a6b51511afade7
Alice Machado Koch <62021549+AliceMachado@users.noreply.github.com>
2021-06-26 20:40:35 -0300

    Update ManipuladorArquivo.java

--- ManipuladorArquivo.java
+++ ManipuladorArquivo.java
@@ -5,52 +5,37 @@ import java.io.*;
 
 public class ManipuladorArquivo{
 	
-	public static String linha = "";
+	public static String linha;
 	public static double linha_num;
-	public static double linha_num2;
 	
 	public static void leitorBubbleSort(String path) throws IOException {
+		//Bubble sort
 		
 		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		linha = buffRead.readLine();
+		
+		ArrayList<Double> numeros = new ArrayList<>();
 		
 		while (true) {
 			if (linha != null) {
 				linha_num = Double.parseDouble(linha);
-				linha_num2 = Double.parseDouble(linha);
-				System.out.println(linha);
+				numeros.add(linha_num);
+		        Arrays.toString(numeros.toArray());
 			} else
 				break;
 				linha = buffRead.readLine();
 			}
-		buffRead.close();
-	}
-	
-	public ManipuladorArquivo() {
-		//Bubble sort
-        ArrayList<Double> numeros = new ArrayList<> ();
-        numeros.add(linha_num);
-        Arrays.toString(numeros.toArray());
-        
-        ArrayList<Double> numeros2 = new ArrayList<> ();
-        numeros2.add(linha_num2);
-        Arrays.toString(numeros2.toArray() );
         
         for (int i = 0; i < numeros.size(); i++) {  
         	numeros.set(i, linha_num);
-            numeros2.set(i, linha_num2);
         }
         for (int i = 0; i < numeros.size(); i++) { 
         	numeros.get(i);
             if(numeros.get(i) > linha_num){
-                linha_num2 = linha_num;
                 linha_num = numeros.get(i);
-            }else{
-                if((numeros.get(i) > linha_num2) && (numeros.get(i) != linha_num)) {
-                	linha_num2 = numeros.get(i);
-                }
             }
         }
+        System.out.println(numeros);
+		buffRead.close();
 	}
-	
-	
 }
