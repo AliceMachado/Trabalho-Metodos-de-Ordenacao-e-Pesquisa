9dbe377926296a088ab06bcb1d9dcd3ea0168219
arthurteske <80133080+arthurteske@users.noreply.github.com>
2021-07-02 16:16:21 -0300

    Update ManipuladorArquivo.java

--- ManipuladorArquivo.java
+++ ManipuladorArquivo.java
@@ -3,21 +3,26 @@ package Ordenacao_e_pesquisa;
 import java.util.*;
 import java.io.*;
 
+import Ordenação.PesquisaLinear;
+
 public class ManipuladorArquivo{
 	
 	public static String linha;
-	public static int linha_num;
+	public static long linha_num;
+	public static boolean terminou = false;
+	static Scanner teclado = new Scanner(System.in);
 	public static ArrayList<Long> numeros = new ArrayList<Long>();
 	
 	public static void leitorInsertionSort(String path) throws IOException {
 		//Insertion sort
-		long j, x;
+		int j;
+		Long x;
 		BufferedReader buffRead = new BufferedReader(new FileReader(path));
 		linha = buffRead.readLine();
-				
+		
 		while (true) {
 			if (linha != null) {
-				linha_num = Integer.parseInt(linha);
+				linha_num = Long.parseLong(linha);
 				numeros.add(linha_num);
 		        Arrays.toString(numeros.toArray());
 			} else
@@ -27,7 +32,7 @@ public class ManipuladorArquivo{
         
       		for (int i = 1; i < numeros.size(); i++) {
 	       		x = numeros.get(i);
-	        	j = i - 1;
+	        	j = (int) (i - 1);
 	            	while (j >= 0 && numeros.get(j) > x) {
 	                	numeros.set(j + 1, numeros.get(j));
 	                	j = j - 1;
@@ -37,7 +42,7 @@ public class ManipuladorArquivo{
 		for(int i = 0; i < numeros.size(); i++) {
 			System.out.println(numeros.get(i));
 		}
-		Pesquisas.pesquisa(null);
+	    Pesquisas.pesquisa(null);
 		buffRead.close();
 	}
 	
@@ -47,12 +52,12 @@ public class ManipuladorArquivo{
 		BufferedReader buffRead = new BufferedReader(new FileReader(path));
 		linha = buffRead.readLine();
 		
-		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		
 		
 		while (true) {
 				if (linha != null) {
-					linha_num = Integer.parseInt(linha);
-					numeros.add(linha_num);
+					linha_num = Long.parseLong(linha);
+					numeros.add((long) linha_num);
 			        Arrays.toString(numeros.toArray());
 				} else
 					break;
@@ -60,7 +65,7 @@ public class ManipuladorArquivo{
 	        	boolean estaOrdenado = true;
 			    for(int j = 0; j < numeros.size() - 1 - i; j++) {
 			    	if(numeros.get(j) > numeros.get(j + 1)) {
-			    		int aux = numeros.get(j);
+			    		Long aux = numeros.get(j);
 			    		numeros.set(j, numeros.get(j + 1));
 			    		numeros.set(j + 1, aux);
 			    		estaOrdenado = false;
@@ -82,8 +87,8 @@ public class ManipuladorArquivo{
         	for(int i = 0; i < numeros.size(); i++) {
 			System.out.println(numeros.get(i));
 		}
-		// serve para chamar o metodo de pesquisa
-		Pesquisas.pesquisa(null);
+    	Pesquisas.pesquisa(null);
 		buffRead.close();
+
 	}
 }
