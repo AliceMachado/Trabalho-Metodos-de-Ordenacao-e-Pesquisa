b173ad0c2ee205f043706b05af7c50fca881d88c
arthurteske <80133080+arthurteske@users.noreply.github.com>
2021-07-02 11:47:56 -0300

    Update Pesquisas.java

--- Pesquisas.java
+++ Pesquisas.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class Pesquisas {
 
 	public static void pesquisa(String[] args) {
+		//isso aqui define qual pesquisa o usuaro deseja realizar
 		Scanner teclado = new Scanner(System.in);
 		int opcao = 0;
 		
 		System.out.println("Por favor, selecione o tipo de pesquisa desejada: "
-				+ "\n 1 - Pesquisa Bin·ria " + "\n 2 - Pequisa Linear" );
+				+ "\n 1 - Pesquisa Bin√°ria " + "\n 2 - Pequisa Linear" );
 		opcao = teclado.nextInt();
 		
 		
 		switch(opcao) {
 		 	case 1: 
 		 		//chama metodo pesquisa binaria
+				//precisa ser adicionado
 		 		break;
 		 	case 2:
+				//chama o metodo de pesquisa linear
 		 		PesquisaLinear.pesquisaLinear(ManipuladorArquivo.numeros);
 		 		break;
 		}		
