bb04001b890396f2f0dcfadef644c88c417bed9a
Alice Machado Koch <62021549+AliceMachado@users.noreply.github.com>
2021-07-03 18:12:18 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -10,7 +10,7 @@ public class Principal {
 		Scanner teclado = new Scanner(System.in);
 		
 		int opcao = 0;
-		String path = "C:\\Users\\familia\\Documents\\alice\\Faculdade\\Programação_de_soluções_computacionais\\Douglas\\Trabalho_Ordenador_de_arquivo\\teste.txt";
+		String path = "C:\\Users\\familia\\Documents\\alice\\Faculdade\\Programação_de_soluções_computacionais\\Douglas\\Trabalho_Ordenador_de_arquivo\\NumerosOrdenarArquivo.txt";
 		
 		System.out.println("---------------Menu---------------");
 		System.out.println("1 - Método de ordenação Insertion Sort");
