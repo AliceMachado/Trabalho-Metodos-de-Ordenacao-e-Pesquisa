132dc9eed7c8bb30a5bf412d346c21f7b9c696ec
Alice Machado Koch <62021549+AliceMachado@users.noreply.github.com>
2021-07-03 17:24:35 -0300

    Update ManipuladorArquivo.java

--- ManipuladorArquivo.java
+++ ManipuladorArquivo.java
@@ -10,44 +10,46 @@ public class ManipuladorArquivo{
 	static Scanner teclado = new Scanner(System.in);
 	public static ArrayList<Long> numeros = new ArrayList<Long>();
 	
-	public static void leitorInsertionSort(String path) throws IOException {
-		//Insertion sort
-		long tempoinicial = System.currentTimeMillis();
-		
-		int j;
-		Long x;
-		BufferedReader buffRead = new BufferedReader(new FileReader(path));
-		linha = buffRead.readLine();
-		
-		while (true) {
-			if (linha != null) {
-				linha_num = Long.parseLong(linha);
-				numeros.add(linha_num);
-		        Arrays.toString(numeros.toArray());
-			} else
-				break;
-				linha = buffRead.readLine();
+	public static class leitorInsertionSort {
+		public static void leitor(String path) throws IOException {
+			//Insertion sort
+			long tempoinicial = System.currentTimeMillis();
+
+			int j;
+			Long x;
+			BufferedReader buffRead = new BufferedReader(new FileReader(path));
+			linha = buffRead.readLine();
+
+			while (true) {
+				if (linha != null) {
+					linha_num = Long.parseLong(linha);
+					numeros.add(linha_num);
+				Arrays.toString(numeros.toArray());
+				} else
+					break;
+					linha = buffRead.readLine();
+				}
+
+			for (int i = 1; i < numeros.size(); i++) {
+				x = numeros.get(i);
+				j = (int) (i - 1);
+				while (j >= 0 && numeros.get(j) > x) {
+					numeros.set(j + 1, numeros.get(j));
+					j = j - 1;
+				}
+				numeros.set(j + 1, x);
 			}
-        
-      		for (int i = 1; i < numeros.size(); i++) {
-	       		x = numeros.get(i);
-	        	j = (int) (i - 1);
-	            	while (j >= 0 && numeros.get(j) > x) {
-	                	numeros.set(j + 1, numeros.get(j));
-	                	j = j - 1;
-	            	}
-	            	numeros.set(j + 1, x);
-        	}
-		for(int i = 0; i < numeros.size(); i++) {
-			System.out.println(numeros.get(i));
+			for(int i = 0; i < numeros.size(); i++) {
+				System.out.println(numeros.get(i));
+			}
+			Pesquisas.pesquisa();
+
+			long tempofinal = System.currentTimeMillis();
+			long tempototal = tempofinal - tempoinicial;
+			System.out.println("Tempo de Processamento de InsertionSort: " + tempototal + "ms");
+
+			buffRead.close();
 		}
-	    	Pesquisas.pesquisa();
-		
-		long tempofinal = System.currentTimeMillis();
-	        long tempototal = tempofinal - tempoinicial;
-	        System.out.println("Tempo de Processamento de InsertionSort: " + tempototal + "ms");
-		
-		buffRead.close();
 	}
 	
 	public static class leitorBubbleSort {
@@ -102,64 +104,63 @@ public class ManipuladorArquivo{
 	}
 	
 	public static class leitorQuickSort{
-        public static void leitor(int ini, int fim) {
-            int i = ini;
-            int f = fim;
-            Long pivo = numeros.get((int) ((i + f) / 2));
-            Long k;
+		public static void leitor(int ini, int fim) {
+		    int i = ini;
+		    int f = fim;
+		    Long pivo = numeros.get((int) ((i + f) / 2));
+		    Long k;
 
-            while (i <= f) {
-                while (numeros.get(i) < pivo) {
-                    i = i + 1;
-                }
-                while (numeros.get(f) > pivo) {
-                    f = f - 1;
-                }
-                if (i <= f) {
-                    k = numeros.get(i);
-                    numeros.set(i, numeros.get(f));
-                    numeros.set(f, k);
-                    i = i + 1;
-                    f = f - 1;
-                }
-            }
-            if (f > ini) {
-                leitor(numeros, ini, f);
-            }
-            if (i < fim) {
-                leitor(numeros, i, fim);
-            }
-            
-            for(int j = 0; j < numeros.size(); j++) {
-                System.out.println(numeros.get(j));
-            }
+		    while (i <= f) {
+			while (numeros.get(i) < pivo) {
+			    i = i + 1;
+			}
+			while (numeros.get(f) > pivo) {
+			    f = f - 1;
+			}
+			if (i <= f) {
+			    k = numeros.get(i);
+			    numeros.set(i, numeros.get(f));
+			    numeros.set(f, k);
+			    i = i + 1;
+			    f = f - 1;
+			}
+		    }
+		    if (f > ini) {
+			leitor(numeros, ini, f);
+		    }
+		    if (i < fim) {
+			leitor(numeros, i, fim);
+		    }
+
+		    for(int j = 0; j < numeros.size(); j++) {
+			System.out.println(numeros.get(j));
+		    }
         }
         
         public static void leitor(String path) throws IOException {
 
-	    long tempoinicial = System.currentTimeMillis();
-		
-            BufferedReader buffRead = new BufferedReader(new FileReader(path));
-            linha = buffRead.readLine();
+		long tempoinicial = System.currentTimeMillis();
 
+		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		linha = buffRead.readLine();
 
-            while (true) {
-                if (linha != null) {
-                    linha_num = Long.parseLong(linha);
-                    numeros.add(linha_num);
-                    Arrays.toString(numeros.toArray());
-                } else
-                    break;
-                    linha = buffRead.readLine();
-                }
-            leitor(numeros, 0, numeros.size() - 1);
-            Pesquisas.pesquisa();
-	    
-	    long tempofinal = System.currentTimeMillis();
-	    long tempototal = tempofinal - tempoinicial;
-	    System.out.println("Tempo de Processamento de QuickSort: " + tempototal + "ms");
-		
-            buffRead.close();
+		while (true) {
+			if (linha != null) {
+				linha_num = Long.parseLong(linha);
+				numeros.add(linha_num);
+				Arrays.toString(numeros.toArray());
+			} else
+				break;
+				linha = buffRead.readLine();
+			}
+		 leitor(numeros, 0, numeros.size() - 1);
+		 Pesquisas.pesquisa();
+
+		 long tempofinal = System.currentTimeMillis();
+		 long tempototal = tempofinal - tempoinicial;
+		 System.out.println("Tempo de Processamento de QuickSort: " + tempototal + "ms");
+
+		 buffRead.close();
         }
     }
 }
